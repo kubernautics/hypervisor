793c42ad789104a2846d1855c999cf92dfcf9a30
usrbinkat <usrbinkat@braincraft.io>
2020-09-13 14:49:04 -0700

    tweaks

--- tools/dev.sh
+++ tools/dev.sh
@@ -4,6 +4,7 @@ sudo podman run -it --rm \
     --entrypoint bash \
     --pull always --name devkit-vpc \
     --volume ${HOME}/.ssh:/root/.ssh:z \
+    --volume ${HOME}/.bashrc:/root/.bashrc:z \
     --workdir /root/platform/iac/shaman/libvirt \
     --volume $(pwd):/root/platform/iac/shaman/libvirt:z \
   docker.io/codesparta/konductor:latest
